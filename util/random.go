9f12b15198e7d0e1ddb9adb1f23c5c6c05c9460a
22120278 <nvhphuc364@gmail.com>
2025-02-20 11:08:59 +0700

    Add utility functions for generating random data

---
+++ util/random.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandOwner() string {
+	n := rand.Intn(6) + 5 
+	name := make([]byte, n)
+	for i := range name {
+		name[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(name)
+}
+
+func RandBalance(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandCurrency() string {
+	currencies := []string{"EUR", "USD", "VND", "YEN"}
+	return currencies[rand.Intn(len(currencies))]
+}
