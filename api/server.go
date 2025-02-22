490e910123f50481d6aa4a24be49184c5a08d6e7
22120278 <nvhphuc364@gmail.com>
2025-02-23 00:24:44 +0700

    Add transfer management: implement create transfer endpoint and register currency validation

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"github.com/hoangphuc3064/MyBank/db/sqlc"
 )
 
@@ -14,10 +16,15 @@ func NewServer(store *sqlc.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validateCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
-
+	
+	router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
 }
