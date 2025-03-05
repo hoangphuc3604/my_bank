706fe0207dc2b9f01fec76a7f56ce9013d348a3f
22120278 <nvhphuc364@gmail.com>
2025-03-05 14:46:17 +0700

    Add server initialization error handling: update server creation to include configuration and handle potential errors

--- main.go
+++ main.go
@@ -21,7 +21,10 @@ func main() {
 	}
 
 	store := sqlc.NewStore(testDB)
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("Cannot create server:", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
